6b47763367bd90777f5a62757f5e11147f8c769f
oscarvik <oscarcv@stud.ntnu.no>
2020-05-18 14:27:15 -0700

    bugfix and colab setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='Language-Modelling-CSE291-AS2',
+    version='0.1.0',
+    packages=find_packages(),
+    license='MIT License',
+)
